b7f73c98b4a392a8d637497ee979ecd05d29f09b
Wildtypers <wildtypers@gmail.com>
2026-06-04 00:42:38 -0700

    creation of base frame of daily event tasks

--- main/lib/calanderPage.dart
+++ main/lib/calanderPage.dart
@@ -3,6 +3,7 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'package:table_calendar/table_calendar.dart';
 
+List<String> task = ["Homework1", "Homework2"];
 
 class calendarPage extends StatelessWidget{
   const calendarPage({super.key});
@@ -24,6 +25,12 @@ class calendarPage extends StatelessWidget{
               firstDay: DateTime.utc(2000, 1, 1),
               lastDay: DateTime.utc(2026, 12, 31),
               focusedDay: DateTime.now(),
+              eventLoader: (DateTime date){
+                return task;
+              },
+              onDaySelected: (selectedDay, focusDay){
+                
+              },
             )
           ]
         )
